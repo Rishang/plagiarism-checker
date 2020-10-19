38a82955b63381afd819e79ac69d1541ce9089f5
Rishang <--add>
2020-10-19 18:49:34 +0530

    added demo script for showing use case

---
+++ demo.py
@@ -0,0 +1,10 @@
+from textGoogleMatch import googleSearch,compareStr
+
+copied_content="""HTML stands for Hyper Text Markup Language. HTML is the standard markup language for Web pages. HTML elements are the building blocks of HTML pages."""
+
+data = googleSearch(copied_content)
+
+for result in data:
+    compare = compareStr(data[result]["description"],copied_content)
+    if compare > 70:
+        print(f'copied data form {data[result]["url"]}')
