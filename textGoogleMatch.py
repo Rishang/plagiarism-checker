da6fac44e247ed0c651050c7b1f7ffe9817f2ae4
Rishang <--add>
2020-10-19 17:31:47 +0530

    added comments to script

--- textGoogleMatch.py
+++ textGoogleMatch.py
@@ -7,14 +7,22 @@ from  selenium.common import exceptions
 options = Options()
 options.headless = True
 
-def search(query):
-    driver = webdriver.Chrome('chromedriver')
+def googleSearch(query):
 
+    # specifing browser web driver
+    driver = webdriver.Chrome('chromedriver')
+    
+    # search query
     search_engine = "https://www.google.com/search?q="
     query = query.replace(" ","+")
+    
+    # driver.get will open browser and enter the string we provide as argument
     driver.get(search_engine + query + "&start=" + "0")
 
+    # all the needed data will be stored here 
+    # which will be returned by this function
     data = {}
+    
     s_len = len(driver.find_elements_by_class_name('g')) + 2
     
     for s_block in range(s_len):
@@ -22,26 +30,30 @@ def search(query):
             # store data collected of each s_block to block {}
             block = {}
 
-            # domain name of web having content
+            # find domain name of web having content
             domain_p = driver.find_element_by_xpath(f"""//*[@id="rso"]/div[{s_block}]/div/div[1]/a/div/cite""")
             domain_p = domain_p.get_attribute('innerText')
+            
+            # regex pattern for selectiong domain name
             pattern =  r"""(https?:\/\/)?(([a-z0-9-_]+\.)?([a-z0-9-_]+\.[a-z0-9-_]+))"""
             domain = re.search(pattern, domain_p)[0]
-            block["domain"] = domain
 
-            # url of content
+            # find url of content
             url = driver.find_element_by_xpath(f"""//*[@id="rso"]/div[{s_block}]/div/div[1]/a""")
-            url = url.get_attribute('href')
-            block["url"] = url
+            url = url.get_attribute('href')            
 
-            # title of content
+            # find title of content
             title = driver.find_element_by_xpath(f"""//*[@id="rso"]/div[{s_block}]/div/div[1]/a/h3/span""")
             title = title.get_attribute("innerText")
-            block["title"] = title
             
-            # description of content
+            # find description of content
             description = driver.find_element_by_xpath(f"""//*[@id="rso"]/div[{s_block}]/div/div[2]/div/span""")
             description = description.get_attribute("innerText")
+            
+            # save all data to block {}
+            block["domain"] = domain
+            block["url"] = url
+            block["title"] = title
             block["description"] = description
 
             # save block dictionary to main dictionary
@@ -54,6 +66,11 @@ def search(query):
     return data
 
 
-def compareSentence(str1,str2):
+def compareStr(str1,str2):
     
+    # difflib.SequenceMatcher will compare 2 argument strings
+    # and show us in form of float number between 0-1
+    # 0 means no match , 1 means full match
+
+    # multiplying result to 100 so it kinda looks like 0-100% based result
     return difflib.SequenceMatcher(None, f"""{str1}""", f"""{str2}""").ratio() * 100
